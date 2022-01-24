79c2361b257735d843af053226864bdee3bf4217
JRitzenhoff <ritzenhoffjlhs@gmail.com>
2022-01-24 10:20:50 -0500

    Rust client initial push

---
+++ project1/client/wordle_guesser/src/lib.rs
@@ -0,0 +1,14 @@
+
+
+pub fn hello_world(name: String) {
+    println!("Hello World {}", name);    
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ project1/client/wordle_guesser/src/main.rs
@@ -0,0 +1,5 @@
+use wordle_guesser;
+
+fn main() {
+    wordle_guesser::hello_world(String::from("Guesser"));
+}
